a817b2427bb1752b3859df2fab946f4dfc9ffb5d
fahmy-dev <faahmy@proton.me>
2025-04-04 18:19:46 +0300

    initial commit

---
+++ server/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from models import db
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+CORS(app)
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return jsonify({'message': 'Welcome to Ambulance API'})
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)

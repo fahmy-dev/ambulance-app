58d5e4303b8c0acd28ed97f5b833cf0a95b9d718
fahmy-dev <faahmy@proton.me>
2025-04-13 22:30:12 +0300

    changed flask port to default

--- server/app.py
+++ server/app.py
@@ -335,5 +335,5 @@ def serve_static(path):
         return send_from_directory('../client/dist', 'index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
   
